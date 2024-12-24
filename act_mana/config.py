bf32dbe3590d55db201f2e10fd513bc881c3447e
xl <wxl_zhile@sina.cn>
2024-12-24 15:59:19 +0800

    update

--- act_mana/config.py
+++
@@ -1,6 +0,0 @@
-
-sentence_transformer_model = '/home/xl/cspace/fft-codes/Robot-Behavior-Planner-App/fft-app/src-tauri/src/weights/all-MiniLM-L6-v2'
-fbpcr_model = '/home/xl/Downloads/models/metamotivo-M-1'
-
-with open('humenv_tasks.txt', 'r') as f:
-    humenv_tasks = [line.strip() for line in f.readlines()]
